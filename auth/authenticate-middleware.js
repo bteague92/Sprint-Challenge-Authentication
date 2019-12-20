efab0d7f1397a12759583221d90cd181f26e769f
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-12-20 11:30:32 -0500

    created auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { token } = req.headers;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || "Is it safe?";
+
+    jwt.verify(token, secret, function (err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: "No shoes, no shirt.......no token" });
+      } else {
+        req.token = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "You shall not pass!" });
+  }
 };
